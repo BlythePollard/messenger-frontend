e8dba44f47a4eca3927522dada4f3a8e95f06050
BlythePollard <github email address>
2020-05-16 12:53:19 -0600

    enabled login and message form on login

--- src/components/UserLogin.js
+++ src/components/UserLogin.js
@@ -1,10 +1,51 @@
 import React from 'react';
+import MessageForm from './MessageForm'
 
 export default class UserLogin extends React.Component {
+    state = {
+        username: '',
+        isSubmitted: false
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            username: event.target.value
+        })
+    }
+
+    handleSubmit = event => {
+        const username = this.state.username
+        event.preventDefault();
+        return fetch('http://localhost:3000/api/v1/users', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+        body: JSON.stringify({username})
+        })
+        .then(resp => resp.json())
+        .then(data => {
+            console.log(data)
+            localStorage.setItem("token", data.id)
+            this.setState({ username: data.username})
+            this.setState({ isSubmitted: true })
+        })
+    }
+
     render() {
         return (
             <div>
-                this is the user login form
+               <form onSubmit={this.handleSubmit}>
+                   <input 
+                        type="username"
+                        placeholder="username"
+                        value={this.state.username}
+                        onChange={this.handleChange}
+                   />
+                   <input type='submit' value='Login'/>
+                </form> 
+                {this.state.isSubmitted && <MessageForm />}           
             </div>
         )
     }
